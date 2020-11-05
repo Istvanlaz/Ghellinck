9879c32a6cdc92963cf2af577779f7c344bfeeb7
Istvan Lazar <istvanlaz@gmail.com>
2020-11-05 16:58:45 +0100

    created the basic stucture of the app --created a first version of the navbar --created routes, models and controllers for products and categories --products are only accessible within predefined categories --created mockup seeds to best develop

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :realisations, :about_me, :contact ]
 
   def home
+    return root_path
+  end
+
+  def realisations
+  end
+
+  def about_me
+  end
+
+  def contact
   end
 end
