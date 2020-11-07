2758e42a85816367e24ba0e86633e13edf0f597d
Istvan Lazar <istvanlaz@gmail.com>
2020-11-07 18:13:56 +0100

    created custom redirect for devise edit registration action reviewed scss for formatting and reviewed naming for ease of code

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
     resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   end
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
   root to: 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
