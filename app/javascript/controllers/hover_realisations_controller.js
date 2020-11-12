e6d0541addc45b7d732056c5a00568036f64bf36
Istvan Lazar <istvanlaz@gmail.com>
2020-11-12 17:00:38 +0100

    small final changes

--- app/javascript/controllers/hover_realisations_controller.js
+++ app/javascript/controllers/hover_realisations_controller.js
@@ -2,36 +2,6 @@ document.addEventListener('turbolinks:load', () => {
   const categoryImages = document.querySelector('.category_imgs');
 
   const realisationsDesktop = () => {
-    // if (categoryImages) {
-    //   const images = Array.from(categoryImages.children);
-    //   const categories = document.querySelector('.category_list');
-    //   const category = Array.from(categories.children);
-
-    //   const setImagePosition = (image, index) => {
-    //     image.style.left = 101 * index + '%';
-    //   };
-    //   images.forEach(setImagePosition);
-
-    //   const moveToImage = (categoryImages, targetImage) => {
-    //     categoryImages.style.transform = 'translateX(-' + targetImage.style.left + ')';
-    //     categoryImages.style.transition = '0.1s ease-in';
-    //   };
-
-    //   // // moves to the correct indicator when clicked
-    //   categories.addEventListener('mouseover', e => {
-    //     // what indicator is clicked on
-    //     const targetCategory = e.target.closest('li');
-
-    //     if (!targetCategory) return;
-
-    //     const targetIndex = category.findIndex(cat => cat === targetCategory);
-
-    //     const targetImage = images[targetIndex];
-
-    //     moveToImage(categoryImages, targetImage);
-    //   });
-    // }
-
     if (categoryImages) {
       const images = Array.from(categoryImages.children);
       const categoryList = document.querySelector('.category_list');
@@ -73,10 +43,24 @@ document.addEventListener('turbolinks:load', () => {
     }
   };
 
+  const realisationsMobile = () => {
+    if (categoryImages) {
+      const images = Array.from(categoryImages.children);
+      const categoryList = document.querySelector('.category_list');
+      const categories = Array.from(categoryList.children);
+
+      const setImageOpacity = (image, index) => {
+        if (index === 0) {
+          image.classList.add('current_slide');
+        }
+      };
+      images.forEach(setImageOpacity);
+    }
+  };
 
   if (window.innerWidth < 600) {
 
-    return;
+    realisationsMobile();
 
   } else {
 
